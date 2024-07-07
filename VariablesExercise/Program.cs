2b7f8e6f90a67a8755e365b2ccec17024c6000a3
Alex Leifermann <aleif92@outlook.com>
2024-07-07 17:43:43 -0500

    First commit for variable assignment

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -4,7 +4,19 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+        
+         //Declaring and initializing variables
+            string dogName = "Upgrade";
+            int dogAge = 2;
+            char startsWith = 'U';
+            bool isAlive = true;
+            double dogYears = 14.5;
+            decimal howFar = 29.99m;
+
+            //Output to console
+            Console.WriteLine($" My dogs name is {dogName}.\n He is {dogAge} years old. That is {dogName} with a {startsWith}.\n If you were to ask me if he is still alive i would say yes that is {isAlive}.\n I think thats like {dogYears} years in dog years.\n He can run {howFar} miles before he gets tierd!");
+
+
         }
     }
 }
